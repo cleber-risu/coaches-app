4284570984e1585546ee0c5faa9437aacdd86b3c
risu <cleber.risu@outlook.com>
2024-08-10 13:43:18 -0300

    working on the contact form

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,3 +1,87 @@
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.message === ''
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+    },
+  },
+};
+</script>
+
 <template>
-  <h2>Contact Coach</h2>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-mail</label>
+      <input type="email" id="email" v-model.trim="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea cols="5" id="message" v-model.trim="message"></textarea>
+    </div>
+    <p class="errors" v-if="!formIsValid">
+      Please enter a valid email and not-empty message.
+    </p>
+    <div class="actios">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
 </template>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
