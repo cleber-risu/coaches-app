f023c532bfb40a30fe35113400bd2225f8283de5
risu <cleber.risu@outlook.com>
2024-08-10 17:39:56 -0300

    sending a put http request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,30 @@
 export default {
-  registerCoach({ commit, rootGetters }, data) {
+  async registerCoach({ commit, rootGetters }, data) {
+    const userId = rootGetters.userId;
     const coachData = {
-      id: rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    commit('registerCoach', coachData);
+
+    const url = `https://vue-http-demo-86775-default-rtdb.firebaseio.com/${userId}.json`;
+
+    const response = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(coachData),
+    });
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error
+    }
+
+    commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
