40a416640ec7027445f566e818043e31121d316e
risu <cleber.risu@outlook.com>
2024-08-14 11:07:01 -0300

    updating the ui based on auth state

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,3 +1,13 @@
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
+  },
+};
+</script>
+
 <template>
   <header>
     <nav>
@@ -7,7 +17,12 @@
       <ul>
         <li>
           <router-link to="/coaches">All Coaches</router-link>
+        </li>
+        <li v-if="isLoggedIn">
           <router-link to="/requests">Requests</router-link>
+        </li>
+        <li v-else>
+          <router-link to="/auth">Login</router-link>
         </li>
       </ul>
     </nav>
